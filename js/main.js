306a652dd413c2412550ed7bcbfbea739bbd8bf8
Nuh Ali <nuh_mali@hotmail.co.uk>
2020-07-20 14:28:25 +0100

    Portfolio updated
    Placeholder projects removed, buttons to actual links. Form Validation started

--- js/main.js
+++ js/main.js
@@ -17,3 +17,38 @@ menuButton.addEventListener('click', () => {
         }
     })
 });
+
+const validate = () => {
+
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let error_message = document.getElementById("error_message");
+    
+    error_message.style.padding = "10px";
+    
+    let text;
+    if(name.length < 4 && name.includes(' ')){
+        text = "Please enter valid full name";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if(isNaN(phone) || phone.length != 10){
+        text = "Please Enter valid Phone Number";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if(email.indexOf("@") == -1 || email.length < 6){
+        text = "Please Enter valid Email";
+        error_message.innerHTML = text;
+        return false;
+    }
+    if(subject.length <= 140){
+        text = "Please Enter More Than 140 Characters";
+        error_message.innerHTML = text;
+        return false;
+    }
+    alert("Form Submitted Successfully!");
+    return true;
+}
